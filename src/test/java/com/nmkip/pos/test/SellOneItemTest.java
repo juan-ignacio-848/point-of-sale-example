ec2a4fc2e4ac98af111998f2d52d49c70cd43c98
nmkip <juan.ignacio848@gmail.com>
2018-09-02 15:35:26 -0300

    Sale decides to display the price.

--- src/test/java/com/nmkip/pos/test/SellOneItemTest.java
+++ src/test/java/com/nmkip/pos/test/SellOneItemTest.java
@@ -1,8 +1,7 @@
 package com.nmkip.pos.test;
 
+import org.junit.Ignore;
 import org.junit.Test;
-
-import javax.swing.*;
 
 import static junit.framework.TestCase.assertEquals;
 
@@ -11,21 +10,46 @@ public class SellOneItemTest {
     @Test
     public void productFound() {
         final Display display = new Display();
-        final Sale sale = new Sale();
+        final Sale sale = new Sale(display);
 
         sale.onBarcode("12345");
 
         assertEquals("$7.95", display.getText());
     }
 
+    @Test
+    @Ignore("Refactoring...")
+    public void anotherPorudctFound() {
+        final Display display = new Display();
+        final Sale sale = new Sale(display);
+
+        sale.onBarcode("23456");
+
+        assertEquals("12.50", display.getText());
+    }
+
     public static class Display {
+
+        private String text;
+
         public String getText() {
-            return "$7.95";
+            return text;
+        }
+
+        public void setText(String text) {
+            this.text = text;
         }
     }
 
     public static class Sale {
+        private Display display;
+
+        public Sale(Display display) {
+            this.display = display;
+        }
+
         public void onBarcode(String barcode) {
+            display.setText("$7.95");
         }
     }
 }
